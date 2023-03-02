8279b3d5f703c905d9c8fd20d2d1f671099697f9
cpgloomy <cpgloomy@gmail.com>
2023-03-02 16:49:54 -0600

    format

--- src/character.h
+++ src/character.h
@@ -1,13 +1,12 @@
 #include <string>
 
-class Character
-{
+class Character {
+ public:
+  Character(){};
+  ~Character(){};
+  std::string GetName() { return character_name; }
+  void setName(std::string name) { character_name = name; }
 
-    public:
-        Character() {};
-        ~Character() {};
-        std::string GetName() { return character_name; }
-        void setName(std::string name) { character_name = name; }
-    private:
-        std::string character_name;
+ private:
+  std::string character_name;
 };
