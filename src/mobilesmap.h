45a70c231cf8d22745df821c8270bfde20ed419a
cpgloomy <cpgloomy@gmail.com>
2023-03-08 11:38:08 -0600

    some stuff dont care

---
+++ src/mobilesmap.h
@@ -0,0 +1,11 @@
+#ifndef MOBILESMAP_H
+#define MOBILESMAP_H
+
+#include <map>
+#include <string>
+
+struct MobilesMap {
+  std::map<std::string, std::string> mobilesMap_;
+};
+
+#endif  //? MOBILESMAP_H
